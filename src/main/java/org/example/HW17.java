81ebdc3f0bec80a615526a1f31f8611c1c55069b
Alex <nevmerzhickiy.an@gmail.com>
2023-11-06 20:57:11 +0200

    Test3

---
+++ src/main/java/org/example/HW17.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class HW17 {
+    public class Test3 {
+        public static void main(String[] args) {
+            int Alex = 7;
+            int Maks = 9;
+
+            Maks++; // Інкрементуємо Maks
+
+            int sum = Alex + Maks; // Знаходимо суму
+
+            System.out.println("Сума значень Alex та Maks: " + sum);
+        }
+    }
+}
---
+++ src/main/java/org/example/Test3.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Test3 {
+    public static void main(String[] args) {
+        // Завдання 1
+        int Alex = 7;
+        int Maks = 9;
+        Maks++; // Інкрементуємо Maks
+
+        int sum = Alex + Maks; // Знаходимо суму
+
+        System.out.println("Сума Alex та Maks: " + sum);
+
+        // Завдання 2
+        int x = 1;
+        int y = 0;
+
+        y = y + 3 * x;
+        // x = x * 2;
+        x = x * 16;
+        // y = y + 2 * x;
+        y = y + x;
+
+
+        System.out.println("Результат y: " + y);
+
+        // Завдання 3
+        System.out.println("It's Java string: C:\\Program Files\\Java\\jdk1.7.0\\bin\\");
+    }
+}
+
